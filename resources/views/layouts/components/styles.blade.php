cc3695a089c97e44e03ea82f431fecc58f23e2de
f4nc1s <frankfracisreal@gmail.com>
2024-11-15 20:22:19 +0100

    Xintra Laravel UI

---
+++ resources/views/layouts/components/styles.blade.php
@@ -0,0 +1,42 @@
+<!-- Meta Data -->
+<meta charset="UTF-8">
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="Description" content="Laravel Bootstrap Responsive Admin Web Dashboard Template">
+<meta name="Author" content="Spruko Technologies Private Limited">
+<meta name="keywords" content="laravel template, laravel, laravel admin, admin bootstrap, laravel admin template, dashboard, admin panel template, laravel framework, admin template, laravel admin panel, admin, laravel dashboard, dashboard for laravel, admin panel for laravel, bootstrap admin panel template.">
+
+<!-- Title-->
+<title> Xintra - Laravel Bootstrap 5 Premium Admin & Dashboard Template </title>
+
+<!-- Favicon -->
+<link rel="icon" href="{{ asset('resources/assets/images/brand-logos/favicon.ico') }}" type="image/x-icon">
+
+<!-- Main Theme Js -->
+<script src="{{ asset('resources/assets/main.js') }}"></script>
+<!-- ICONS CSS -->
+<link href="{{ asset('resources/assets/icon-fonts/icons.css') }}" rel="stylesheet">
+<!-- Choices JS -->
+<script src="{{ asset('resources/assets/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+<!-- Bootstrap Css -->
+<link id="style" href="{{ asset('resources/assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" >
+<!-- Node Waves Css -->
+<link href="{{ asset('resources/assets/libs/node-waves/waves.min.css') }}" rel="stylesheet" > 
+<!-- Simplebar Css -->
+<link href="{{ asset('resources/assets/libs/simplebar/simplebar.min.css') }}" rel="stylesheet" >
+
+<!-- Color Picker Css -->
+<link rel="stylesheet" href="{{ asset('resources/assets/libs/flatpickr/flatpickr.min.css') }}">
+<link rel="stylesheet" href="{{ asset('resources/assets/libs/@simonwep/pickr/themes/nano.min.css') }}">
+<!-- Choices Css -->
+<link rel="stylesheet" href="{{ asset('resources/assets/libs/choices.js/public/assets/styles/choices.min.css') }}">
+<!-- FlatPickr CSS -->
+<link rel="stylesheet" href="{{ asset('resources/assets/libs/flatpickr/flatpickr.min.css') }}">
+<!-- Auto Complete CSS -->
+<link rel="stylesheet" href="{{ asset('resources/assets/libs/@tarekraafat/autocomplete.js/css/autoComplete.css') }}">
+
+<!-- APP CSS & APP SCSS -->
+<link rel="preload" as="style" href="{{ asset('resources/assets/app-BXaKe1N-.css') }}" /><link rel="stylesheet" href="{{ asset('resources/assets/app-BXaKe1N-.css') }}" />
+
+<!-- FlatPickr CSS -->
+<link rel="stylesheet" href="{{ asset('resources/assets/libs/flatpickr/flatpickr.min.css') }}">
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light" data-menu-styles="dark" data-toggled="close">
+    <head>
+        @include('layouts.components.styles')
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
